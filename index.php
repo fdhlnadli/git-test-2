56b40f54db55d86eb095439e73e9e55a50437e06
fdhlnadli <fadhlansatrio15@gmail.com>
2021-12-13 19:04:24 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo cumque voluptate beatae esse iusto magni alias minima possimus similique amet.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
