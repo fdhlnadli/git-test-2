7a43fce36a0173fa63e85682b81ad99bc111926b
fdhlnadli <fadhlansatrio15@gmail.com>
2021-12-13 18:58:35 +0700

    Menambahkan Paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo cumque voluptate beatae esse iusto magni alias minima possimus similique amet.</p>
 </body>
 </html>
